4bc0264a5f00d196ab04d8b11b0509cf6df3f058
rajuahmmed111 <ahraju2003@gmail.com>
2024-10-02 11:23:26 +0600

    gym class server

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+    },
+  },
+  {
+    ignores: ["**/node_modules/", "**/dist/"],
+  },
+];
